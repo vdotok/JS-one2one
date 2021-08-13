ec3d5ba70958641137d3b5a0e596a6c411353d85
Mukhtiar <muhammad.mukhtiar@norgic.com>
2021-08-13 18:58:28 +0500

    dmain name change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl : 'https://tenant-api.vdotok.com/API/v0/'
+  apiBaseUrl : 'https://tenant-api.vdotok.dev/API/v0/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiBaseUrl : 'https://tenant-api.vdotok.com/API/v0/'
+  apiBaseUrl : 'https://tenant-api.vdotok.dev/API/v0/'
 };
 
 /*
