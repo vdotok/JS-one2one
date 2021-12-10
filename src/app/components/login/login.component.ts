92f0909f3febb81e47082dc437adb0fb5508cde8
Summen Zahid <summen.zahid@norgic.com>
2021-12-10 16:45:57 +0500

    updated

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -45,7 +45,7 @@ export class LoginComponent implements OnInit, OnDestroy {
     FormsHandler.validateForm(this.loginForm);
     if (this.loginForm.invalid) return;
     const saveData = this.loginForm.value;
-    saveData.project_id = '125YLEY1';
+    saveData.project_id = '170JRJUI';
     this.loading = true;
     this.formError = null;
     this.auth.login(saveData).subscribe(v => {
